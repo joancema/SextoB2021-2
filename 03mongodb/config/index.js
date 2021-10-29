3f78365687746872209f69592d478d4c918d2c2a
John Cevallos <joancemac@gmail.com>
2021-10-29 12:46:03 -0500

    cronjob

---
+++ 03mongodb/config/index.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV !== "production" )
+{
+    require("dotenv").config();
+}
+
+module.exports={
+    MONGO_URI: process.env.MONGO_URI
+}
+
+
+
+
--- 03mongodb/ejemplo.js
+++ 03mongodb/ejemplo.js
@@ -1,20 +1,16 @@
 const mongoose = require("mongoose");
-const conexion= "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/sextob?retryWrites=true&w=majority"
+const conexion= "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/sextob?retryWrites=true&w=majority";
+( async ()=>{
+    const estado= await mongoose.connect(conexion);
+    const Usuario =  mongoose.model("Usuario", {nombre:String});
+    const usuario1=  new Usuario({nombre:"Prueba2"});
+    const guardoUsuario= await usuario1.save();
+    const resultado= await Usuario.find();
+    console.log(resultado)
+})();
 
 
-mongoose.connect(conexion).then(respuesta=>{
-    //console.log(respuesta)
-}).catch(error=>{
-    //console.log(error);
-})
 
-const Usuario =  mongoose.model("Usuario", {nombre:String});
-
-const usuario1=  new Usuario({nombre:"Leixer Lucas"});
-
-usuario1.save();
-
-Usuario.find().then(console.log);
 
 
 
