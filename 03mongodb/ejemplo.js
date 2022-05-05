b8c1aa2c4129ff3c35e85d0e48d2253d5c631251
John Cevallos <joancemac@gmail.com>
2022-05-05 11:20:38 -0500

    relaciones

--- 03mongodb/ejemplo.js
+++ 03mongodb/ejemplo.js
@@ -1,13 +1,26 @@
 const mongoose = require("mongoose");
 // const conexion= "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/sextob?retryWrites=true&w=majority";
-const conexion= "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/Milita?retryWrites=true&w=majority";
+const conexion= "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/Militao?retryWrites=true&w=majority";
 ( async ()=>{
     const estado= await mongoose.connect(conexion);
-    const Usuario =  mongoose.model("Usuario", {nombre:String});
-    const usuario1=  new Usuario({nombre:"Prueba2"});
-    const guardoUsuario= await usuario1.save();
-    const resultado= await Usuario.find();
-    console.log(resultado)
+    const Grupo = mongoose.model("Grupo", {nombre:String});
+    const Usuario =  mongoose.model("Usuario", {nombre:String, idgrupo:{ type:mongoose.Schema.Types.ObjectId, ref:"Grupo" }});
+
+    
+    const grupo1=  new Grupo({nombre:"Grupo 1"});
+    
+    const guardoGrupo= await grupo1.save();
+    
+    console.log(guardoGrupo)
+    console.log(guardoGrupo._id)
+    
+    const usuario1=  new Usuario({nombre:"Usuario1", idgrupo:guardoGrupo._id});
+
+     const guardoUsuario= await usuario1.save();
+     console.log(guardoUsuario)
+
+    // const resultado= await Usuario.find();
+    // console.log(resultado)
 })();
 
 
