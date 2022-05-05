7bdf87e9c9b4801ffd59541fa2c65cae4963b6ec
John Cevallos <joancemac@gmail.com>
2022-05-05 14:27:58 -0500

    detalle en consulta de datos

--- 03mongodb/ejemplo.js
+++ 03mongodb/ejemplo.js
@@ -4,23 +4,52 @@ const conexion= "mongodb+srv://joancema:jacm1310@cluster0.x7met.mongodb.net/Mili
 ( async ()=>{
     const estado= await mongoose.connect(conexion);
     const Grupo = mongoose.model("Grupo", {nombre:String});
-    const Usuario =  mongoose.model("Usuario", {nombre:String, idgrupo:{ type:mongoose.Schema.Types.ObjectId, ref:"Grupo" }});
+    const Permiso = mongoose.model("Permiso", {nombre:String});
+    const Usuario =  mongoose.model("Usuario",
+    {nombre:String, 
+    idgrupo:{ type:mongoose.Schema.Types.ObjectId, ref:"Grupo"},
+    // permisos: [{ type:mongoose.Schema.Types.ObjectId, ref:"Permiso" }]
+    permisos: [{ permiso:{ type:mongoose.Schema.Types.ObjectId, ref:"Permiso" }, estado:{type: Boolean}}]
+});
 
     
     const grupo1=  new Grupo({nombre:"Grupo 1"});
+    const permiso1=  new Permiso({nombre:"Permiso 1"});
+    const permiso2=  new Permiso({nombre:"Permiso 2"});
     
     const guardoGrupo= await grupo1.save();
     
-    console.log(guardoGrupo)
-    console.log(guardoGrupo._id)
+    // console.log(guardoGrupo)
+    // console.log(guardoGrupo._id)
+
+    const guardoPermiso1= await permiso1.save();
+    const guardoPermiso2= await permiso2.save();
+
+    // console.log(guardoPermiso1._id)
+    // console.log(guardoPermiso2._id)
     
-    const usuario1=  new Usuario({nombre:"Usuario1", idgrupo:guardoGrupo._id});
+    // const usuario1=  new Usuario
+    // (
+    //     {nombre:"Usuario1",
+    //      idgrupo:guardoGrupo._id,
+    //     permisos:[guardoPermiso1._id,guardoPermiso2._id]});
+
+    const usuario1=  new Usuario
+    (
+        {nombre:"Usuario1",
+         idgrupo:guardoGrupo._id,
+        permisos:[
+             { permiso:guardoPermiso1._id , estado:true },
+             { permiso:guardoPermiso2._id , estado:false },
+        ]});
 
      const guardoUsuario= await usuario1.save();
-     console.log(guardoUsuario)
+    //  console.log(guardoUsuario)
 
-    // const resultado= await Usuario.find();
-    // console.log(resultado)
+     const resultado= await Usuario.find()
+     .populate("idgrupo")
+     .populate("permisos.permiso");
+     console.log(resultado[14].permisos)
 })();
 
 
